263e2c596fcc12348c5103fc2da63aff61c3aa64
BenJ1337 <benjaminhacker1995@googlemail.com>
2020-01-14 20:06:18 +0100

    readme:)

--- modules/01 - Text/output.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$rex_values_content = json_decode(rex_article_slice::getArticleSliceById(REX_SLICE_ID)->getValue(2), true);
-	echo '<div class="text-block">'.$rex_values_content['cke5_editor'].'</div>';
-?>
-
---
+++ readme.md
@@ -0,0 +1,6 @@
+### RedaxoCMS Onepager Template
+
+Simples Redaxo Grundgerüst mit Bootstrap 4.1. für Onepager 
+
+Abhängigkeiten:
+- MForm ( https://github.com/FriendsOfREDAXO/mform )
